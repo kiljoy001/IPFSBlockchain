57522364d6e50e34f09f702441495529a1e2566d
kiljoy001 <kiljoy001@gmail.com>
2018-05-31 01:59:48 -0700

    Created Block Class with no methods

---
+++ IPFSBlockchain/Block Primatives/Block.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IPFSBlockchain.Block_Primatives
+{
+    public class Block
+    {
+        /// <summary>
+        /// Implements block in blockchain
+        /// Hash & Previous has exposed through properties
+        /// </summary>
+        /// 
+        private string _hash;
+        private string _previoushash;
+        private List<string> _data;
+        private long timestamp;
+
+        //Constructor
+        public Block(List<string> data, string previousHash)
+        {
+            _data = data;
+            _previoushash = previousHash;
+            long epochTicks = new DateTime(1970, 1, 1,0,0,0, DateTimeKind.Utc).Ticks;
+            timestamp = epochTicks / TimeSpan.TicksPerSecond;
+        }
+
+        //Properties
+        public string Hash { get { return _hash; } }
+        public string LastHash { get { return _previoushash; }  }
+    }
+}
