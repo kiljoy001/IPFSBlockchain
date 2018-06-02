cea516c30208b560c2f9bcb3b1335c3610914bbe
kiljoy001 <kiljoy001@gmail.com>
2018-06-02 04:52:57 -0700

    Added  Blockchain constructor, add method and validity boolean check

--- IPFSBlockchain/Block Primatives/Blockchain.cs
+++ IPFSBlockchain/Block Primatives/Blockchain.cs
@@ -4,8 +4,38 @@ using System.Text;
 
 namespace IPFSBlockchain.Block_Primatives
 {
-    class Blockchain
+    public class Blockchain
     {
+        static private LinkedList<Block> Chain;
+        public Blockchain(Block firstBlock)
+        {
+            LinkedListNode<Block> node = new LinkedListNode<Block>(firstBlock);
+            Chain.AddLast(firstBlock);
+        }
+
+        public void Add(Block block)
+        {
+            Chain.AddLast(block);
+        }
+
+        public bool IsValid()
+        {
+            LinkedListNode<Block> node = Chain.Last;
+            LinkedListNode<Block> lastNode = node.Previous;
+            if(node.Value.Hash != node.Value.CalculateHash())
+            {
+                return false;
+            }
+            else if(lastNode.Value.Hash != node.Value.LastHash)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
 
     }
 }
